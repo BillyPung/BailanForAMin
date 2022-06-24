6229be01cf7b2a57c2159b7fd7c047e73dd63738
Billy Peng <BillyPeng@foxmail.com>
2022-06-25 02:25:09 +0800

    2:25

--- BailanForAMin/Assets/Shengyang/StartMenuButtonControl.cs
+++ BailanForAMin/Assets/Shengyang/StartMenuButtonControl.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenuButtonControl : MonoBehaviour
 {
+    private Button StartGameButton, ExitGameButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGameButton = GameObject.Find("StartButton").GetComponent<Button>();
+        StartGameButton.onClick.AddListener(StartOnClick);
+        ExitGameButton = GameObject.Find("ExitButton").GetComponent<Button>();
+        ExitGameButton.onClick.AddListener(ExitOnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
          
+    }
+
+    void StartOnClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void ExitOnClick()
+    {
+        Application.Quit();
     }
 }
