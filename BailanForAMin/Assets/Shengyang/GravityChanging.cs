0ea1dc27c93e04329a6835251cd50783ac65a632
Billy Peng <BillyPeng@foxmail.com>
2022-06-25 04:00:37 +0800

    optimized Gravity

--- BailanForAMin/Assets/Shengyang/GravityChanging.cs
+++ BailanForAMin/Assets/Shengyang/GravityChanging.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 public class GravityChanging : MonoBehaviour
 {
     public float gravityMultiplier = 9.8f;
+    public float rotateWaitTime = 10F, rotateCoolDownTime = 12F;
+    private float coolDownTimeCounter = 0;
 
     private void Start()
     {
@@ -16,15 +18,21 @@ public class GravityChanging : MonoBehaviour
 
     private void Update()
     {
-        print(Physics2D.gravity);
-        if (Input.GetKeyDown(KeyCode.N))
+        //print(Physics2D.gravity);
+        coolDownTimeCounter += Time.deltaTime;
+        if (coolDownTimeCounter > rotateCoolDownTime)
         {
-            //print("You pushed N");
-            doGravityChanging(true);
-        }
-        else if (Input.GetKeyDown(KeyCode.M))
-        {
-            doGravityChanging(false);
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                //print("You pushed N");
+                coolDownTimeCounter = 0;
+                doGravityChanging(true);
+            }
+            else if (Input.GetKeyDown(KeyCode.M))
+            {
+                coolDownTimeCounter = 0;
+                doGravityChanging(false);
+            }
         }
     }
 
@@ -33,18 +41,42 @@ public class GravityChanging : MonoBehaviour
     {
         //print("doGravityAll");
         //rotateCamera(turnClockwise);
+        StartCoroutine(rotateWaiter(turnClockwise));
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         gravityChanging(turnClockwise);
     }
 
     void rotateCamera(bool turnClockwise)
     {
+        // Camera.main.transform.rotation = Quaternion.Euler(Vector2.forward * degrees);
+        //Transform cameraTrans = Camera.main.transform;
+        //cameraTrans.eulerAngles = Vector3.forward * degrees;
+        // transform.eulerAngles = Vector3.forward * degrees;
+        //transform.RotateAround (transform.position, Vector3.forward, degrees);
+        //StartCoroutine(rotateWaiter(turnClockwise));
+    }
+
+    private IEnumerator rotateWaiter(bool turnClockwise)
+    {
         int degrees = 90;
         if (turnClockwise) degrees = -degrees;
-        // Camera.main.transform.rotation = Quaternion.Euler(Vector2.forward * degrees);
-        Transform cameraTrans = Camera.main.transform;
-        //cameraTrans.eulerAngles = Vector3.forward * degrees;
-       // transform.eulerAngles = Vector3.forward * degrees;
-       transform.RotateAround (transform.position, Vector3.forward, degrees);
+        float counter = 0;
+        //Rotate 90 deg
+        //transform.Rotate(new Vector3(90, 0, 0), Space.World);
+
+        //Wait for 4 seconds
+
+        while (counter < rotateWaitTime)
+        {
+            //Increment Timer until counter >= waitTime
+            counter += Time.deltaTime;
+            transform.RotateAround(transform.position, Vector3.forward, degrees * Time.deltaTime);
+            //Debug.Log("We have waited for: " + counter + " seconds");
+            //Wait for a frame so that Unity doesn't freeze
+            //Check if we want to quit this function
+
+            yield return null;
+        }
     }
 
     void gravityChanging(bool turnClockWise)
@@ -52,8 +84,8 @@ public class GravityChanging : MonoBehaviour
         //print("doGravityChanging");
         //Vector2 targetGravity = new Vector2();
         Vector2 currentGravity = Physics2D.gravity;
-        
-        //print(currentGravity);
+
+        print(currentGravity);
         if (currentGravity == Vector2.down * gravityMultiplier)
         {
             print("Now the gravity is down");
