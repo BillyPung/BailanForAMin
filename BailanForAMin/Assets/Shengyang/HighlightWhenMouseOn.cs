c064b55dc6e4703c6626e28fb12d96c8b768c8d4
Billy Peng <BillyPeng@foxmail.com>
2022-06-25 15:28:33 +0800

    highlight

---
+++ BailanForAMin/Assets/Shengyang/HighlightWhenMouseOn.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightWhenMouseOn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Color startcolor;
+    public SpriteRenderer rend;
+    void Start()
+    {
+       rend = GetComponent<SpriteRenderer>();
+    }
+
+    // The mesh goes red when the mouse is over it...
+    void OnMouseEnter()
+    {
+        /*print("mouse enter");
+        print(rend.material.color);*/
+        if (StaticVariable.pasueTime) rend.color = Color.yellow;
+        //print("now color is " + rend.material.color);
+    }
+
+    // ...the red fades out to cyan as the mouse is held over...
+    /*void OnMouseOver()
+    {
+        rend.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+    }*/
+
+    // ...and the mesh finally turns white when the mouse moves away.
+    void OnMouseExit()
+    {
+        if (StaticVariable.pasueTime) rend.color = Color.white;
+    }
+}
