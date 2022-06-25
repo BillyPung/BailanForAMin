849764d3fda4d5d94f33322bbd46ae9263dde6a3
gcctbilly <gcctxhl@163.com>
2022-06-25 23:01:31 +0800

    first scence

--- BailanForAMin/Assets/Shengyang/deathFloorScript.cs
+++ BailanForAMin/Assets/Shengyang/deathFloorScript.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 
 public class deathFloorScript : MonoBehaviour
 {
+    ShowDeathUI showDeathUI;
     // Start is called before the first frame update
-
-    private void OnTriggerEnter(Collider other)
+    void Start()
     {
-        if (other.gameObject.tag == "Player")
+        showDeathUI = GetComponent<ShowDeathUI>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.CompareTag("Player"))
         {
             //black screen and transport to saved location
+            showDeathUI.showDeathUI();
             other.transform.position = StaticVariable.savedLocation;
         }
     }
