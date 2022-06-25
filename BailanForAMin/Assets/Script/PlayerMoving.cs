92ba95c079ab5b56ba0b01f38066644fc464d487
gcctbilly <gcctxhl@163.com>
2022-06-25 09:35:42 +0800

    Add jump and freeze anima
    Add jump and freeze anima

--- BailanForAMin/Assets/Script/PlayerMoving.cs
+++ BailanForAMin/Assets/Script/PlayerMoving.cs
@@ -16,19 +16,22 @@ public class PlayerMoving : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myFeet = GetComponent<BoxCollider2D>();
+        myAnim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         flip();
         run();
+        jump();
+        freeze();
         checkGrounded();
+        switchAnimation();
     }
 
     void checkGrounded()
     {
         isGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
-        Debug.Log(isGround);
     }
 
     void flip()
@@ -53,8 +56,50 @@ public class PlayerMoving : MonoBehaviour
         if (isGround)
         {
             float moveDir = Input.GetAxis("Horizontal");
-            Vector2 playerVel = new Vector2(moveDir * runSpeed, jumpSpeed);
+            Vector2 playerVel = new Vector2(moveDir * runSpeed, myRigidbody.velocity.y);
             myRigidbody.velocity = playerVel;
+            bool playerHasXAxisSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+            myAnim.SetBool("walk", playerHasXAxisSpeed);
+        }
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGround )
+        {
+            myAnim.SetBool("jumpUp", true);
+            Vector2 playerVel = new Vector2(0, jumpSpeed);
+            myRigidbody.velocity = playerVel;
+        }
+    }
+    
+    void switchAnimation()
+    {
+        myAnim.SetBool("idle", false);
+        if(myAnim.GetBool("jumpUp"))
+        {
+            if (myRigidbody.velocity.y < 0.0f)
+            {
+                myAnim.SetBool("jumpUp", false);
+                myAnim.SetBool("jumpDown", true);
+            }
+        }
+        else if(isGround)
+        {
+            myAnim.SetBool("jumpDown", false);
+            myAnim.SetBool("idle", true);
+        }
+    }
+
+    void freeze()
+    {
+        if(isGround)
+        {
+            myRigidbody.constraints = RigidbodyConstraints2D.None;
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            myRigidbody.constraints = RigidbodyConstraints2D.FreezePositionX;
         }
     }
 }
